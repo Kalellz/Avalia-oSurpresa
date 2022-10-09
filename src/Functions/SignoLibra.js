f5b6961548d930221c4e2674d025f9ea94ecd08a
dieguitozz <diegolenda2006@gmail.com>
2022-10-08 21:35:03 -0300

    test commit

--- src/Functions/SignoLibra.js
+++ src/Functions/SignoLibra.js
@@ -1,7 +1,10 @@
 function SignoLibra(mes, dia) {
     if (mes != 'OUTUBRO' && mes != 'SETEMBRO') {
         return 'Não'
-    } else if (mes === 'SETEMBRO' && dia > 22 && dia <= 30) return 'Sim'
+    } 
+    else if (mes === 'SETEMBRO' && dia > 22 && dia <= 30) return 'Sim'
     else if (mes === 'OUTUBRO' && dia <= 22) return 'Sim'
     else return 'Não'
-} export default SignoLibra;
+
+} 
+export default SignoLibra;
