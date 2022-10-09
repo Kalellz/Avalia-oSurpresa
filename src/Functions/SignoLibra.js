8aa96675e6500f731f15410bbd2d39c8141e0875
kalel <ZoroPlay1220@gmail.com>
2022-10-08 21:30:51 -0300

    ultimo update pt2

--- src/Functions/SignoLibra.js
+++ src/Functions/SignoLibra.js
@@ -1,7 +1,7 @@
 function SignoLibra(mes, dia) {
-    if (mes != 'Outubro' && mes != 'Setembro') {
+    if (mes != 'OUTUBRO' && mes != 'SETEMBRO') {
         return 'Não'
-    } else if (mes === 'Setembro' && dia > 22 && dia <= 30) return 'Sim'
-    else if (mes === 'Outubro' && dia <= 22) return 'Sim'
+    } else if (mes === 'SETEMBRO' && dia > 22 && dia <= 30) return 'Sim'
+    else if (mes === 'OUTUBRO' && dia <= 22) return 'Sim'
     else return 'Não'
 } export default SignoLibra;
--- src/Pages/Signo/App.js
+++ src/Pages/Signo/App.js
@@ -6,14 +6,13 @@ function App() {
     const [dia, setDia] = useState()
     const [mes, setMes] = useState()
     const [resultado, setResultado] = useState()
-
     return (
         <main>
             <h1>Você é de Libra?</h1>
-            <section>
+            <section className='VA-Content'>
                 <input type="number" class="form-control" placeholder="Dia" value={dia} onChange={(e) => setDia(e.target.value)} />
                 <input type="text" class="form-control" placeholder="Mes" value={mes} onChange={(e) => setMes(e.target.value)} />
-                <button type="button" class="btn btn-primary" onClick={() => setResultado(SignoLibra(mes, dia))}>Verificar</button>
+                <button type="button" class="btn btn-primary" onClick={() => setResultado(SignoLibra(mes.toUpperCase(), dia))}>Verificar</button>
             </section>
             <h1>{resultado}</h1>
         </main>
