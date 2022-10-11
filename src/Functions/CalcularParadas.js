a67ad8b93e79ec24870e876d93c7842d2cefa967
dieguitozz <diegolenda2006@gmail.com>
2022-10-11 12:19:17 -0300

    messages on pages

--- src/Functions/CalcularParadas.js
+++ src/Functions/CalcularParadas.js
@@ -9,7 +9,11 @@ export default function CalcularParadas(tanque, consumo, distancia) {
     return 'Informe uma distância válida';
     let precisa = distancia / consumo;
     let qtdParadas = precisa / tanque;
-
     qtdParadas = Math.ceil(qtdParadas);
-    return qtdParadas;
+
+    if(qtdParadas === 1)
+        return `${`Você precisará parar para abastecer ${qtdParadas} vez`}`
+
+    let msg = `${`Você precisará parar para abastecer ${qtdParadas} vezes`}`
+    return msg;
 }
--- src/Functions/CalcularSalario.js
+++ src/Functions/CalcularSalario.js
@@ -3,5 +3,6 @@ export default function CalcularSalario(bruto, bonus, desconto) {
         return 'Salário Bruto Inválido'
     let acrescimo = bruto * bonus / 100;
     let total = bruto - desconto + acrescimo;
-    return total;
+    let msg = 'O valor do seu salário líquido é de R$ ' + total;
+    return msg;
 }
