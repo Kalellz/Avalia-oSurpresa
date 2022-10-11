2f4fd94695ba66b23a9cb94542f1635ecdfc0f41
dieguitozz <diegolenda2006@gmail.com>
2022-10-11 12:06:48 -0300

    hopotermia

--- src/Functions/CalcularFebre.js
+++ src/Functions/CalcularFebre.js
@@ -3,5 +3,5 @@ export default function CalcularFebre(Temperatura){
     else if(Temperatura >= 39.6 && Temperatura < 41) return 'Febre Alta';
     else if(Temperatura >= 37.6 && Temperatura < 39.6) return 'Febre';
     else if(Temperatura >= 36 && Temperatura < 37.6) return 'Normal';
-    else if(Temperatura < 36) return 'Hoportermia';
+    else if(Temperatura < 36) return 'Hiportermia';
 } 
