52ac7f97c5ea5a07b109d3910ae024b1660c6012
dieguitozz <diegolenda2006@gmail.com>
2022-10-11 12:02:55 -0300

    bug fix

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,11 @@ import VendaAcai from './Pages/VendaAcai/App.js';
 import Signo from './Pages/Signo/App.js';
 import Sorveteria from './Pages/Sorveteria/App.js';
 import Salario from './Pages/Salario/App.js';
+<<<<<<< HEAD
 import Paradas from './Pages/Paradas/App.js';
+=======
+import Febre from './Pages/Febre/App.js';
+>>>>>>> 490ed0d4e2b93b552341df3056ddb74775edc75e
 
 export default function Index() {
 	return (
@@ -15,7 +19,11 @@ export default function Index() {
 				<Route path="/Signo" element={<Signo />} />
 				<Route path="/Sorveteria" element={<Sorveteria />} />
 				<Route path="/Salario" element={<Salario />} />
+<<<<<<< HEAD
 				<Route path="/Paradas" element={<Paradas/>} />
+=======
+				<Route path="/Febre" element={<Febre />} />
+>>>>>>> 490ed0d4e2b93b552341df3056ddb74775edc75e
 			</Routes>
 		</BrowserRouter>
 	);
