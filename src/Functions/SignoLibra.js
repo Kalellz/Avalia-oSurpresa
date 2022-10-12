d442a727ac0ad78b3650578ca57d50f035c78054
Vitor <menezes.victorsantos@outlook.com>
2022-10-12 10:26:56 -0300

    Update input type date

--- src/Functions/SignoLibra.js
+++ src/Functions/SignoLibra.js
@@ -1,8 +1,9 @@
-function SignoLibra(mes, dia) {
-	if (mes != "OUTUBRO" && mes != "SETEMBRO") {
-		return "Não";
-	} else if (mes === "SETEMBRO" && dia >= 23 && dia <= 30) return "Sim";
-	else if (mes === "OUTUBRO" && dia <= 22 && dia >= 1) return "Sim";
+function SignoLibra(date) {
+	const newMonth = date.slice(5, 7);
+	const newDay = date.slice(8, 11);
+	if (newMonth != 10 && newMonth != 9) return "Não";
+	else if (newMonth == 9 && newDay >= 23 && newDay <= 30) return "Sim";
+	else if (newMonth == 10 && newDay <= 22 && newDay >= 1) return "Sim";
 	else return "Não";
 }
 export default SignoLibra;
--- src/Pages/Signo/App.js
+++ src/Pages/Signo/App.js
@@ -3,21 +3,19 @@ import { useState } from "react";
 import SignoLibra from "../../Functions/SignoLibra";
 
 function App() {
-	const [dia, setDia] = useState();
-	const [mes, setMes] = useState();
-	const [resultado, setResultado] = useState();
-	return (
-		<main>
-			<h1>Você é de Libra?</h1>
-			<section className="VA-Content">
-				<input type="number" class="form-control" placeholder="Dia" value={dia} onChange={(e) => setDia(e.target.value)} />
-				<input type="text" class="form-control" placeholder="Mes" value={mes} onChange={(e) => setMes(e.target.value)} />
-				<button type="button" class="btn btn-primary" onClick={() => setResultado(SignoLibra(mes.toUpperCase(), dia))}>
-					Verificar
-				</button>
-			</section>
-			<h1>{resultado}</h1>
-		</main>
-	);
+    const [resultado, setResultado] = useState();
+    const [date, setDate] = useState();
+    return (
+        <main>
+            <h1>Você é de Libra?</h1>
+            <section className="VA-Content">
+                <input type="date" class="form-control" placeholder="Data" value={date} onChange={(e) => setDate(e.target.value)} />
+                <button type="button" class="btn btn-primary" onClick={() => setResultado(SignoLibra(date))}>
+                    Verificar
+                </button>
+            </section>
+            <h1>{resultado}</h1>
+        </main>
+    );
 }
 export default App;
