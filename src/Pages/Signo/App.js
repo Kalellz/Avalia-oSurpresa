ad3ea9b60de7d922b080969c1ab342d01a792f6d
Vitor <menezes.victorsantos@outlook.com>
2022-10-12 10:36:22 -0300

    Add Toast to Libra.

--- src/Pages/Signo/App.js
+++ src/Pages/Signo/App.js
@@ -1,16 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import SignoLibra from "../../Functions/SignoLibra";
-
+import toast, { Toaster } from "react-hot-toast";
 function App() {
     const [resultado, setResultado] = useState();
     const [date, setDate] = useState();
     return (
-        <main>
+		<main>
+			<Toaster />
             <h1>Você é de Libra?</h1>
             <section className="VA-Content">
                 <input type="date" class="form-control" placeholder="Data" value={date} onChange={(e) => setDate(e.target.value)} />
-                <button type="button" class="btn btn-primary" onClick={() => setResultado(SignoLibra(date))}>
+				<button type="button" class="btn btn-primary" onClick={() => {
+					toast.loading("Calculando resultado...")
+					setTimeout(() => {
+						toast.dismiss();
+						toast.success("Resultado Calculado!")
+						setResultado(SignoLibra(date))
+					}, 1500)
+				}}>
                     Verificar
                 </button>
             </section>
