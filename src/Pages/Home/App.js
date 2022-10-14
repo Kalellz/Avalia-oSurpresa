ffd76cef8f63f260b7c5b8ae392517bb665ecf22
kalel <ZoroPlay1220@gmail.com>
2022-10-14 15:16:36 -0300

    fim

--- src/Pages/Home/App.js
+++ src/Pages/Home/App.js
@@ -4,28 +4,28 @@ function App() {
 	return (
 		<main className="home-main">
 			<section className="Home-Content">
-				<a type="button" class="btn btn-outline-dark w-75" href="/VendaAcai">
+				<a type="button" class="btn btn-outline-light w-75" href="/VendaAcai">
 					Prova 01 - Venda Açai
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Signo">
+				<a type="button" class="btn btn-outline-light w-75" href="/Signo">
 					Prova 02 - Signo Libra
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Sorveteria">
+				<a type="button" class="btn btn-outline-light w-75" href="/Sorveteria">
 					Prova 03 - Sorveteria
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Salario">
+				<a type="button" class="btn btn-outline-light w-75" href="/Salario">
 					Prova 04 - Salário
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Paradas">
+				<a type="button" class="btn btn-outline-light w-75" href="/Paradas">
 					Prova 05 - Paradas
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Febre">
+				<a type="button" class="btn btn-outline-light w-75" href="/Febre">
 					Prova 06 - Febre
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Familia">
+				<a type="button" class="btn btn-outline-light w-75" href="/Familia">
 					Prova 07 - Familia
 				</a>
-				<a type="button" class="btn btn-outline-dark w-75" href="/Ingresso">
+				<a type="button" class="btn btn-outline-light w-75" href="/Ingresso">
 					Prova 08 - Ingressos
 				</a>
 			</section>
