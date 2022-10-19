ba5aeac3bdda27091a2aea0d6c0dddccbbe6d09b
Vitor <menezes.victorsantos@outlook.com>
2022-10-19 15:15:25 -0300

    Function CalcularLitros

---
+++ src/Functions/CalcularLitros.js
@@ -0,0 +1,10 @@
+function cafe(estudantes, minimoLitros, mililitros) {
+	let multiplo = 1;
+	let litros = (estudantes * mililitros) / 1000;
+	let calc = minimoLitros * multiplo;
+	while (calc < litros) {
+		multiplo++;
+		calc = minimoLitros * multiplo;
+	}
+	return calc;
+}
