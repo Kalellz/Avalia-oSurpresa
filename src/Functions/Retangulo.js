64bbd55d01b8dc86aa5e55efcd54f3fc68e8b70b
Vitor <menezes.victorsantos@outlook.com>
2022-10-19 14:47:27 -0300

    Update Retangulo.js (square and circle added)

--- src/Functions/Retangulo.js
+++ src/Functions/Retangulo.js
@@ -1,10 +1,28 @@
-export default function CalcularRetangulo(base, altura){
+export default function CalcularRetangulo(base, altura, char) {
     let mat = [];
     let arr = [];
-    for (let i = 0; i < base; i++) {
-        arr[i] = "*";
-        for (let j = 0; j < altura; j++) {
-            mat[j] = arr
+    if (char == "Circulo") {
+        for (let i = 0; i < base; i++) {
+            arr[i] = "◉";
+            for (let j = 0; j < altura; j++) {
+                mat[j] = arr
+            }
+        }
+    }
+    else if (char == "Quadrado") {
+        for (let i = 0; i < base; i++) {
+            arr[i] = "▢";
+            for (let j = 0; j < altura; j++) {
+                mat[j] = arr
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < base; i++) {
+            arr[i] = "*";
+            for (let j = 0; j < altura; j++) {
+                mat[j] = arr
+            }
         }
     }
     return mat;
--- src/Pages/Retangulo/App.js
+++ src/Pages/Retangulo/App.js
@@ -3,25 +3,80 @@ import { useState } from "react";
 import CalcularRetangulo from "../../Functions/Retangulo";
 function App() {
 	const [base, setBase] = useState();
-    const [altura, setAltura] = useState();
+	const [altura, setAltura] = useState();
 	const [resultado, setResultado] = useState([]);
+	const [text, setText] = useState("Selecione o caractere");
+	const [char, setChar] = useState("")
 	return (
 		<main>
 			<h1>Desenhar um retangulo</h1>
 
 			<section className="VA-Content">
 				<label>
-					base <input type="number" class="form-control" value={base} onChange={(e) => {setBase(e.target.value)}}/>
+					base{" "}
+					<input
+						type="number"
+						class="form-control"
+						value={base}
+						onChange={(e) => {
+							setBase(e.target.value);
+						}}
+					/>
 				</label>
-                <label>
-					altura <input type="number" class="form-control" value={altura} onChange={(e) => {setAltura(e.target.value)}}/>
+				<label>
+					altura{" "}
+					<input
+						type="number"
+						class="form-control"
+						value={altura}
+						onChange={(e) => {
+							setAltura(e.target.value);
+						}}
+					/>
 				</label>
-				<button type="button" class="btn btn-primary" onClick={() => setResultado(CalcularRetangulo(base, altura))}>
+				<div class="dropdown">
+					<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						{text}
+					</button>
+					<ul class="dropdown-menu">
+						<li>
+							<a class="dropdown-item" onClick={() => {
+								setChar("")
+								setText('Caractere Selecionado: "*"')
+							}}>
+								Usar: "*"
+							</a>
+						</li>
+						<li>
+							<a class="dropdown-item" onClick={() => {
+								setChar("Circulo")
+								setText('Caractere Selecionado: "◉"')
+							}}>
+								Usar: "◉"
+							</a>
+						</li>
+						<li>
+							<a class="dropdown-item" onClick={() => {
+								setChar("Quadrado")
+								setText('Caractere Selecionado: "▢"')
+							}}>
+								Usar: "▢"
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<button type="button" class="btn btn-primary" onClick={() => setResultado(CalcularRetangulo(base, altura, char))}>
 					Verificar
 				</button>
 			</section>
 
-			<h1> {resultado.map((item) => <h1>{item}</h1>)} </h1>
+			<h1>
+				{" "}
+				{resultado.map((item) => (
+					<h1>{item}</h1>
+				))}{" "}
+			</h1>
 		</main>
 	);
 }
